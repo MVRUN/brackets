a18042b7cc0d5dd03102cfb236bfc0b041de0669
MVRUN <55558413+MVRUN@users.noreply.github.com>
2021-02-24 11:22:36 +0300

    feat
    Add a solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let marker = true;
+  while (marker) {
+      let currStrLength = str.length;
+      for(let i = 0; i < bracketsConfig.length; i++) {
+         str = str.replace(bracketsConfig[i][0]+bracketsConfig[i][1], "")
+      }
+      if (currStrLength > str.length) {
+          marker = true;
+      }
+      else {
+          marker = false
+      } 
+  }
+  if (str.length > 0) {
+      return false
+  }
+  else {
+      return true
+  }
 }
